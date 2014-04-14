af179acf4dc1ef8bad7b35a595142721dfd99da6
Rafael Freitas da Silva <rfl.fsilva@gmail.com>
2014-04-14 09:00:06 -0300

    Created q2.c and .h stubs

---
+++ q2.c
@@ -0,0 +1,9 @@
+/*
+Rafael Freitas da Silva 6480585
+
+Prova final, questão 2.
+Função que recebe duas listas L1 e L2, as combina numa lista L3
+e devolve a mediana da lista L3.
+*/
+
+int devolvemediana();
---
+++ q2.h
@@ -0,0 +1,14 @@
+/*
+Rafael Freitas da Silva NUSP 6480585
+
+q2.h
+
+Arquivo cabeçalho correspondente à biblioteca q2.c
+
+*/
+
+#include <stdio.h>
+#ifndef H_Q2
+#define H_Q2
+
+#endif // H_Q2
